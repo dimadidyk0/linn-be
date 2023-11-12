d7baf5f3c77937dd7bbe5a57238141ac0fcc96eb
dimadidyk0 <dimadidyk0@gmail.com>
2023-11-12 22:42:14 +0100

    feat: prepare for using database

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -9,7 +9,6 @@ import {
   Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
@@ -18,34 +17,34 @@ import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
-  @Get()
-  getTasks(@Query() filterDto?: GetTasksFilterDto): Task[] {
-    if (Object.keys(filterDto).length) {
-      return this.tasksService.getTasksWithFilters(filterDto);
-    }
-    return this.tasksService.getAllTasks();
-  }
+  // @Get()
+  // getTasks(@Query() filterDto?: GetTasksFilterDto): Task[] {
+  //   if (Object.keys(filterDto).length) {
+  //     return this.tasksService.getTasksWithFilters(filterDto);
+  //   }
+  //   return this.tasksService.getAllTasks();
+  // }
 
-  @Get('/:id')
-  getTaskById(@Param('id') id: string): Task {
-    return this.tasksService.getTaskById(id);
-  }
+  // @Get('/:id')
+  // getTaskById(@Param('id') id: string): Task {
+  //   return this.tasksService.getTaskById(id);
+  // }
 
-  @Post()
-  createTask(@Body() createTaskDto: CreateTaskDto): Task {
-    return this.tasksService.createTask(createTaskDto);
-  }
+  // @Post()
+  // createTask(@Body() createTaskDto: CreateTaskDto): Task {
+  //   return this.tasksService.createTask(createTaskDto);
+  // }
 
-  @Patch('/:id')
-  updateTask(
-    @Param('id') id: string,
-    @Body() updateTaskDto: UpdateTaskDto,
-  ): Task {
-    return this.tasksService.updateTask(id, updateTaskDto);
-  }
+  // @Patch('/:id')
+  // updateTask(
+  //   @Param('id') id: string,
+  //   @Body() updateTaskDto: UpdateTaskDto,
+  // ): Task {
+  //   return this.tasksService.updateTask(id, updateTaskDto);
+  // }
 
-  @Delete('/:id')
-  deleteTask(@Param('id') id: string): void {
-    this.tasksService.deleteTask(id);
-  }
+  // @Delete('/:id')
+  // deleteTask(@Param('id') id: string): void {
+  //   this.tasksService.deleteTask(id);
+  // }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,77 +1,60 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Task, TaskStatus } from './task.model';
-import { v4 as uuid } from 'uuid';
+import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
-  private tasks: Task[] = [];
-
-  getAllTasks(): Task[] {
-    return this.tasks;
-  }
-
-  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-    const { status, search } = filterDto;
-    return this.tasks.filter((task) => {
-      if (status && task.status !== status) {
-        return false;
-      }
-
-      if (
-        search &&
-        !task.title.toLowerCase().includes(search.toLocaleLowerCase())
-      ) {
-        return false;
-      }
-
-      return true;
-    });
-  }
-
-  getTaskById(id: string): Task {
-    const found = this.tasks.find((task: Task) => task.id === id);
-
-    if (!found) {
-      throw new NotFoundException(`Task with id ${id} is not found`);
-    }
-
-    return found;
-  }
-
-  deleteTask(id: string): void {
-    const isExist: boolean = Boolean(this.getTaskById(id));
-    if (isExist) {
-      this.tasks = this.tasks.filter((task: Task) => task.id !== id);
-    }
-  }
-
-  updateTask(id: string, updateTaskDto: UpdateTaskDto): Task {
-    const existingTaskIndex = this.tasks.findIndex((task) => task.id === id);
-
-    if (existingTaskIndex === -1) {
-      throw new NotFoundException(`Task with id ${id} is not found`);
-    }
-
-    const updatedTask = { ...this.tasks[existingTaskIndex], ...updateTaskDto };
-    this.tasks[existingTaskIndex] = updatedTask;
-
-    return updatedTask;
-  }
-
-  createTask(createTaskDto: CreateTaskDto): Task {
-    const { title, description } = createTaskDto;
-    const task: Task = {
-      id: uuid(),
-      title,
-      description,
-      status: TaskStatus.OPEN,
-    };
-
-    this.tasks.push(task);
-
-    return task;
-  }
+  // getAllTasks(): Task[] {
+  //   return this.tasks;
+  // }
+  // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+  //   const { status, search } = filterDto;
+  //   return this.tasks.filter((task) => {
+  //     if (status && task.status !== status) {
+  //       return false;
+  //     }
+  //     if (
+  //       search &&
+  //       !task.title.toLowerCase().includes(search.toLocaleLowerCase())
+  //     ) {
+  //       return false;
+  //     }
+  //     return true;
+  //   });
+  // }
+  // getTaskById(id: string): Task {
+  //   const found = this.tasks.find((task: Task) => task.id === id);
+  //   if (!found) {
+  //     throw new NotFoundException(`Task with id ${id} is not found`);
+  //   }
+  //   return found;
+  // }
+  // deleteTask(id: string): void {
+  //   const isExist: boolean = Boolean(this.getTaskById(id));
+  //   if (isExist) {
+  //     this.tasks = this.tasks.filter((task: Task) => task.id !== id);
+  //   }
+  // }
+  // updateTask(id: string, updateTaskDto: UpdateTaskDto): Task {
+  //   const existingTaskIndex = this.tasks.findIndex((task) => task.id === id);
+  //   if (existingTaskIndex === -1) {
+  //     throw new NotFoundException(`Task with id ${id} is not found`);
+  //   }
+  //   const updatedTask = { ...this.tasks[existingTaskIndex], ...updateTaskDto };
+  //   this.tasks[existingTaskIndex] = updatedTask;
+  //   return updatedTask;
+  // }
+  // createTask(createTaskDto: CreateTaskDto): Task {
+  //   const { title, description } = createTaskDto;
+  //   const task: Task = {
+  //     id: uuid(),
+  //     title,
+  //     description,
+  //     status: TaskStatus.OPEN,
+  //   };
+  //   this.tasks.push(task);
+  //   return task;
+  // }
 }
