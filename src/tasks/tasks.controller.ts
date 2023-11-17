bc4da2975b2262bcf1bbb3488c7dcc8c9b9173dd
dimadidyk0 <dimadidyk0@gmail.com>
2023-11-17 21:25:06 +0100

    feat: rewrite get tasks method

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -18,13 +18,10 @@ import { Task } from './task.entity';
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
-  // @Get()
-  // getTasks(@Query() filterDto?: GetTasksFilterDto): Task[] {
-  //   if (Object.keys(filterDto).length) {
-  //     return this.tasksService.getTasksWithFilters(filterDto);
-  //   }
-  //   return this.tasksService.getAllTasks();
-  // }
+  @Get()
+  getTasks(@Query() filterDto?: GetTasksFilterDto): Promise<Task[]> {
+    return this.tasksService.getTasks(filterDto);
+  }
 
   @Get('/:id')
   getTaskById(@Param('id') id: string): Promise<Task> {
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,4 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
@@ -26,24 +25,10 @@ export class TasksService {
     return this.tasksRepository.createTask(createTaskDto);
   }
 
-  // getAllTasks(): Task[] {
-  //   return this.tasks;
-  // }
-  // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-  //   const { status, search } = filterDto;
-  //   return this.tasks.filter((task) => {
-  //     if (status && task.status !== status) {
-  //       return false;
-  //     }
-  //     if (
-  //       search &&
-  //       !task.title.toLowerCase().includes(search.toLocaleLowerCase())
-  //     ) {
-  //       return false;
-  //     }
-  //     return true;
-  //   });
-  // }
+  getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    return this.tasksRepository.getTasks(filterDto);
+  }
+
   async deleteTask(id: string): Promise<void> {
     const result = await this.tasksRepository.delete(id);
 
