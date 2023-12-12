a7b3471c48903ba7caad6c899129ce44fd53bb4d
dimadidyk0 <dimadidyk0@gmail.com>
2023-12-12 22:23:45 +0100

    feat: add jwt stratefy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersRepository } from './users.repository';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private userRepository: UsersRepository) {
+    super({
+      secretOrKey: 'topSecret51',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+}
